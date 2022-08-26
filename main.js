9af7061f83f544063f86d5fc316d2408b1ffcd30
henry-thiongi <boblilhot@gmail.com>
2022-08-26 05:34:17 +0300

    completed assignment

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+const heart = {
+  '♡':FULL_HEART,
+  '♥':EMPTY_HEART
 }
+// Your JavaScript code goes here!
+const like = document.querySelectorAll(' footer > ul > li > span')
+
+console.log(like)
+for (let i =0;i<like.length;i++){
+  like[i].addEventListener('click',(e)=>{
+    mimicServerCall()
+    .then(success=>{
+      alert(success)
+      like[i].innerText = heart[like[i].innerText]
+
+       })
+    .catch(error => {
+      document.querySelector('#modal').classList.remove()
+
+      alert(error)
+      setTimeout(()=>document.querySelector('#modal').classList.add('hidden'),3*1e3)
+      })  //if you double click w/o check of server-alert
+   })
+
+}
+
+
+
+
